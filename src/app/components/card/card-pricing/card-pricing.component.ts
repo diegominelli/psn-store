9b6d86d15810dfeb926945dba9317ed5cfc5e294
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2023-10-11 14:37:19 -0300

    desenvolvendo e estilizando o card pricing

--- src/app/components/card/card-pricing/card-pricing.component.ts
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-pricing',
   templateUrl: './card-pricing.component.html',
-  styleUrls: ['./card-pricing.component.scss']
+  styleUrls: ['./card-pricing.component.scss'],
 })
 export class CardPricingComponent {
-
+  @Input()
+  gameType: string = 'Digital PS4';
+  @Input()
+  gamePrice: string = 'R$ 399,90';
 }
