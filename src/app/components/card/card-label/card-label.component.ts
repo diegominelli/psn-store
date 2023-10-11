b29a918753da2f5db9ac05303304e41d2888f802
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2023-10-11 14:15:10 -0300

    desenvolvendo e estilizando o card label

--- src/app/components/card/card-label/card-label.component.ts
+++ src/app/components/card/card-label/card-label.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-label',
   templateUrl: './card-label.component.html',
-  styleUrls: ['./card-label.component.scss']
+  styleUrls: ['./card-label.component.scss'],
 })
 export class CardLabelComponent {
-
+  @Input()
+  gameLabel: string = '';
 }
